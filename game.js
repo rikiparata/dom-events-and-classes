8e59374c31a4680d533ba95343f959d3cc11d32d
Riki Parata <you@example.com>
2018-05-10 21:26:10 +1200

    Game Final Update

--- game.js
+++ game.js
@@ -42,7 +42,16 @@ function updateCounts () {
   }
   
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
-
+var dotsTotal = document.getElementsByClassName('board')[0].children;
+  for (var i = 0; i < dotsTotal.length; i++) {
+    if(dotsTotal[i].classList.contains('blue')) {
+      totals.blue++;
+  } else if (dotsTotal[i].classList.contains('green')) {
+    totals.green++;
+  } else if (dotsTotal[i].classList.contains('invisible')) {
+    totals.invisible++;
+  }
+  }
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
 }
